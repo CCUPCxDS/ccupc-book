c12b589174a94baa414ddd74693a3f8cc7a8f164
syzygy608 <hankwu99@gmail.com>
2025-08-05 18:50:26 +0800

    fix typo

--- vercel.sh
+++ vercel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 下載並解壓 mdbook
-curl -Lo mbbook.tar.gz https://github.com/rust-lang/mdBook/releases/download/v0.4.52/mdbook-v0.4.52-x86_64-unknown-linux-musl.tar.gz;
-tar xvzf mbbook.tar.gz
+curl -Lo mdbook.tar.gz https://github.com/rust-lang/mdBook/releases/download/v0.4.52/mdbook-v0.4.52-x86_64-unknown-linux-musl.tar.gz;
+tar xvzf mdbook.tar.gz
 
 # 下載並解壓 katex plugin
 curl -Lo mdbook-katex.tar.gz https://github.com/lzanini/mdbook-katex/releases/download/0.9.3-binaries/mdbook-katex-v0.9.3-x86_64-unknown-linux-musl.tar.gz
@@ -12,4 +12,4 @@ tar xvzf mdbook-katex.tar.gz
 curl -Lo mdbook-admonish.tar.gz https://github.com/tommilligan/mdbook-admonish/releases/download/v1.20.0/mdbook-admonish-v1.20.0-x86_64-unknown-linux-musl.tar.gz
 tar xvzf mdbook-admonish.tar.gz
 
-./mbbook build
+./mdbook build
