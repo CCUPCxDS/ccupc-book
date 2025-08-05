e482a461112f12dea96effd0752a3983cde9ea69
syzygy608 <hankwu99@gmail.com>
2025-08-05 18:43:50 +0800

    rename sh

--- vercel.sh
+++ vercel.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 # 下載並解壓 mdbook
 curl -Lo mbbook.tar.gz https://github.com/rust-lang/mdBook/releases/download/v0.4.52/mdbook-v0.4.52-x86_64-unknown-linux-musl.tar.gz;
@@ -12,3 +11,5 @@ tar xvzf mdbook-katex.tar.gz
 # 下載並解壓 admonish plugin
 curl -Lo mdbook-admonish.tar.gz https://github.com/tommilligan/mdbook-admonish/releases/download/v1.20.0/mdbook-admonish-v1.20.0-x86_64-unknown-linux-musl.tar.gz
 tar xvzf mdbook-admonish.tar.gz
+
+./mbbook build
